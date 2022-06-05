7fa5daa20f95214c423bed8a915dbc60771f31b3
Roman Chechytokin <romanchechyotkin@gmail.com>
2022-06-05 13:12:51 +0300

    a

--- js/script.js
+++ js/script.js
@@ -1,41 +1,45 @@
-const link = 'http://api.weatherstack.com/current?access_key=f53b140b537e9b3b86281e9afb393703'
-const root = document.querySelector('.app')
-
 let store = {
     city: 'Minsk',
     humidity: 0,
-    observation_time: "00:00 AM",
     pressure: 0,
     temperature: 0,
     visibility: 0,
     weather_descriptions: '',
 }
 
+const weatherLink = `https://api.openweathermap.org/data/2.5/weather?q=${store.city}&appid=c56a5d8b64c3562162d45f98a120efa8`
+const timeLink = 'http://worldtimeapi.org/api/timezone/Europe/'
+const root = document.querySelector('.app')
+
 const fetchData = async () => {
-    const result = await fetch(`${link}&query=${store.city}`)
-    const data = await result.json()
+    const weatherResult = await fetch(weatherLink)
+    const timeResult = await fetch(`${timeLink}` + `${store.city}`)
+
+    const weatherData = await weatherResult.json()
+    const timeData = await timeResult.json()
     
-    console.log(data);
+    console.log(weatherData);
 
     const{
-        current: {
+        visibility,
+        main: {
             humidity,
-            observation_time: observationTime,
             pressure,
-            temperature,
-            visibility,
-            weather_descriptions: weatherDescriptions,
+            temp,
         }
-    } = data
+    } = weatherData
+
+    const{
+        datetime: dateTime
+    } = timeData
 
     store = {
         ...store,
         humidity,
-        observationTime,
         pressure,
-        temperature,
+        temp,
         visibility,
-        weatherDescriptions
+        dateTime    
     }
 
     render()
@@ -46,25 +50,24 @@ const markup = () => {
     const {
         city, 
         humidity,
-        observationTime,
+        dateTime,
         pressure,
-        temperature,
+        temp,
         visibility,
-        weatherDescriptions,
     } = store
 
     return `
-            <div class="top morning">
+            <div class="top ${getBg(cutTime(dateTime))}">
                 <div class="left">
                     <div class="city-title">${city}</div>
                     <div class="descriptions">
                         <img class="icon" src="rain.jpg" alt="icon">
-                        ${weatherDescriptions}
+                        
                     </div>
-                    <div class="temprature">${temperature}°C</div>
+                    <div class="temprature">${translateTemp(temp)}°C</div>
                 </div>
                 <div class="right">
-                    <div class="time">${observationTime}</div>
+                    <div class="time">${cutTime(dateTime)}</div>
                 </div>
             </div>
 
@@ -89,6 +92,27 @@ const markup = () => {
 
 const render = () => {
     root.innerHTML = markup()
+}
+const cutTime = time => {
+    time = time.slice(11, 16)
+    return time
+}
+const translateTemp = (temp) => {
+    temp = Math.round(temp - 273.15)
+    return temp
+}
+const getBg = (time) => {
+    let hour = time.slice(0, 2)
+    let num = Number(hour)
+    if(num >= 6 && num <= 9){
+        return 'morning'
+    }else if(num >= 10 && num <= 16){
+        return 'day'
+    }else if(num >= 17 && num <= 22){
+        return 'evening'
+    }else{
+        return 'night'
+    }
 }
 
 fetchData()
